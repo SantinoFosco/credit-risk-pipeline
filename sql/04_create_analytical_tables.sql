512c26b243c64d47bc54f5919e20059edbdf2715
SantinoFosco <santinofosco@gmail.com>
2026-09-02 19:30:21 -0300

    Queries de creacion de tablas de datos analiticos basadas en informacion de raw_table

--- sql/04_create_analytical_tables.sql
+++ sql/04_create_analytical_tables.sql
@@ -1,4 +1,5 @@
---- Porcentaje de cartera por segmento de riesgo
+--- Creacion de tabla analitica para datos por segmento de riesgo
+CREATE OR REPLACE TABLE `credit-risk-analytics-506721.credit_risk_analytics.risk_segment_data` AS
 SELECT
     risk_segment,
     COUNT(*) AS amount,
@@ -7,21 +8,24 @@ SELECT
 FROM `credit-risk-analytics-506721.credit_risk_analytics.raw_credit_data`
 GROUP BY risk_segment
 
---- Tasa de default global
+---Creacion de tabla analitica para tasa de default global
+CREATE OR REPLACE TABLE `credit-risk-analytics-506721.credit_risk_analytics.global_default_data` AS
 SELECT ROUND(AVG(CAST(default_flag AS INT)) * 100, 2) AS global_default_rate
 FROM `credit-risk-analytics-506721.credit_risk_analytics.raw_credit_data`
 
---- Tasa de default por region
+--- Creacion de tabla analitica para tasa de default por region
+CREATE OR REPLACE TABLE `credit-risk-analytics-506721.credit_risk_analytics.region_default_data` AS
 SELECT region, ROUND(AVG(CAST(default_flag AS INT)) * 100, 2) AS region_default_rate
 FROM `credit-risk-analytics-506721.credit_risk_analytics.raw_credit_data`
 GROUP BY region
-ORDER BY region_default_rate DESC
 
---- Clientes con riesgo de caer en default
+--- Creacion de tabla analitica para clientes con riesgo de caer en default
+CREATE OR REPLACE TABLE `credit-risk-analytics-506721.credit_risk_analytics.high_risk_customers_data` AS
 SELECT COUNTIF(late_payments_12m >= 3 AND default_flag = FALSE) AS high_risk_customers
 FROM `credit-risk-analytics-506721.credit_risk_analytics.raw_credit_data`
 
---- Combinacion de variables con mas tasa de default
+--- Creacion de tabla analitica para combinacion de variables con mas tasa de default
+CREATE OR REPLACE TABLE `credit-risk-analytics-506721.credit_risk_analytics.risk_category_data` AS
 SELECT CASE 
 WHEN late_payments_12m >= 3 AND debt_to_income_ratio > 0.43 THEN 'Very High Risk'
 WHEN late_payments_12m >= 3 AND debt_to_income_ratio <= 0.43 THEN 'High Risk'
@@ -30,4 +34,3 @@ ELSE 'Low Risk' END AS risk_category,
 ROUND(AVG(CAST(default_flag AS INT)) * 100, 2) AS default_rate
 FROM `credit-risk-analytics-506721.credit_risk_analytics.raw_credit_data`
 GROUP BY risk_category
-ORDER BY default_rate DESC
